5999dcb5e355aff5385a46a12bd4c18b2d5632e2
jmis <jmis.dev@gmail.com>
2010-10-03 21:34:13 -0400

    Additional attribution.

--- ClojureExtension/Editor/AutoIndent/ClojureSmartIndent.cs
+++ ClojureExtension/Editor/AutoIndent/ClojureSmartIndent.cs
@@ -1,4 +1,20 @@
-﻿using System.Collections.Generic;
+﻿/********************************************************************************
+*    Copyright (c) ThorTech, L.L.C.. All rights reserved.
+*    The use and distribution terms for this software are covered by the
+*    GNU General Public License, version 2
+*    (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) with classpath
+*    exception (http://www.gnu.org/software/classpath/license.html)
+*    which can be found in the file GPL-2.0+ClasspathException.txt at the root
+*    of this distribution.
+*    By using this software in any fashion, you are agreeing to be bound by
+*    the terms of this license.
+*    You must not remove this notice, or any other, from this software.
+*******************************************************************************
+*    Author: Frank Failla
+*    Modified By: jmis
+*******************************************************************************/
+
+using System.Collections.Generic;
 using Antlr.Runtime;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
